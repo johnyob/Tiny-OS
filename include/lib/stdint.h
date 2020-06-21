ade3a2b74bf819cf9bd91b55a40767cc6ce3597e
Alistair O'Brien <johnyob132@gmail.com>
2020-06-21 19:00:05 +0100

    Implement some standard libc header files

--- include/lib/stdint.h
+++ include/lib/stdint.h
@@ -13,13 +13,35 @@ typedef signed char         int8_t;
 typedef signed short int    int16_t;
 typedef signed int          int32_t;
 typedef signed long         int64_t;
+typedef signed long long    int128_t;
+
+typedef int128_t            intmax_t;
 
 typedef unsigned char       uint8_t;
 typedef unsigned short int  uint16_t;
 typedef unsigned int        uint32_t;
 typedef unsigned long       uint64_t;
+typedef unsigned long long  uint128_t;
+
+typedef uint128_t           uintmax_t;
 
 typedef int64_t     intptr_t;
 typedef uint64_t    uintptr_t;
+
+#define MAX(a, b)                   \
+({                                  \
+    __typeof__ (a) _a = (a);        \
+    __typeof__ (b) _b = (b);        \
+    _a > _b ? _a : _b;              \
+})
+
+#define MIN(a, b)                   \
+({                                  \
+    __typeof__ (a) _a = (a);        \
+    __typeof__ (b) _b = (b);        \
+    _a < _b ? _a : _b;              \
+})
+
+#define ABS(x)
 
 #endif //TINY_OS_STDINT_H
---
+++ include/lib/stdlib.h
@@ -0,0 +1,12 @@
+//
+// Created by Alistair O'Brien on 6/21/2020.
+//
+
+#ifndef TINY_OS_STDLIB_H
+#define TINY_OS_STDLIB_H
+
+static inline int abs(int n) {
+    return n < 0 ? -(n) : n;
+}
+
+#endif //TINY_OS_STDLIB_H
