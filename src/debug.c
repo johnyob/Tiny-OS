2e6a5b741d5528baf607a8273af0420abbdcaa9c
Alistair O'Brien <johnyob132@gmail.com>
2020-06-24 14:36:47 +0100

    Update debug.c. Added header and comments

--- src/debug.c
+++ src/debug.c
@@ -1,12 +1,32 @@
-//
-// Created by Alistair O'Brien on 6/21/2020.
-//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// - Alistair O'Brien - 6/21/2020 - University of Cambridge
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//      File:        debug.c
+//      Environment: Tiny OS
+//      Description: debug implements the kernel_panic method which prints the location of the kernel panic
+//                   and then prints what caused the kernel panic. We then enter an infinite loop.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 #include <lib/stdbool.h>
 #include <lib/stdarg.h>
 #include <lib/stdio.h>
 
 #include <debug.h>
 
+/*
+ * Procedure:   kernel_panic
+ * -------------------------
+ * This procedure, once invoked causes the kernel to panic, which consists of printing
+ * some important debugging information followed by entering an infinite loop.
+ *
+ * @char* file:     The filename where the panic has occurred. e.g. pmm.c
+ * @int line:       The line of the file where the panic occurred.
+ * @char* function: The function where the panic occurred. e.g. alloc_page
+ * @char* message:  A message indicating what caused the panic e.g. assertion blah failed.
+ * @...:            Additional formatting arguments for the debugging message [message].
+ *
+ */
 void kernel_panic(const char* file, int line, const char* function, const char* message, ...) {
     va_list va;
     va_start(va, message);
