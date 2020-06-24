682a82cccee4ae64ed947e4708b103bafa062b51
Alistair O'Brien <johnyob132@gmail.com>
2020-06-24 14:34:23 +0100

    Update debug.h. Fixed macros and added header.

--- include/debug.h
+++ include/debug.h
@@ -1,6 +1,11 @@
-//
-// Created by Alistair O'Brien on 6/20/2020.
-//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// - Alistair O'Brien - 6/20/2020 - University of Cambridge
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//      File:        debug.h
+//      Environment: Tiny OS
+//      Description: Contains the kernel_panic procedure prototype and a number of assorted debugging
+//                   macros such as info, warn, panic, assert, etc.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef TINY_OS_DEBUG_H
 #define TINY_OS_DEBUG_H
@@ -9,14 +14,14 @@
 
 void kernel_panic(const char* file, int line, const char* function, const char* message, ...);
 
-#define panic(...) kernel_panic(__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define panic(...) kernel_panic(__FILE__, __LINE__, __func__, #__VA_ARGS__)
 #define assert(x)                               \
     if (!(x)) {                                 \
         panic("assertion '%s' failed.", #x);    \
     }
 
-#define info(format, ...) printf("\x1b[0;36m[INFO]\x1b[0m " format, __VA_ARGS__)
-#define warn(format, ...) printf("\x1b[0;33m[WARN]\x1b[0m " format, __VA_ARGS__)
+#define info(format, ...) printf("\x1b[0;36m[INFO]\x1b[0m " format, #__VA_ARGS__)
+#define warn(format, ...) printf("\x1b[0;33m[WARN]\x1b[0m " format, #__VA_ARGS__)
 
 #define NOT_REACHABLE panic("executed an unreachable statement")
 
