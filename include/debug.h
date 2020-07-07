751be2ee2d59608afee7abac12c47abdca57dda5
Alistair <johnyob132@gmail.com>
2020-07-07 18:27:40 +0100

    Update debug.h. Fixed some macro bugs.

--- include/debug.h
+++ include/debug.h
@@ -14,14 +14,14 @@
 
 void kernel_panic(const char* file, int line, const char* function, const char* message, ...);
 
-#define panic(...) kernel_panic(__FILE__, __LINE__, __func__, #__VA_ARGS__)
+#define panic(...) kernel_panic(__FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #define assert(x)                               \
     if (!(x)) {                                 \
-        panic("assertion '%s' failed.", #x);    \
+        panic("assertion %s failed.", #x);      \
     }
 
-#define info(format, ...) printf("\x1b[0;36m[INFO]\x1b[0m " format, #__VA_ARGS__)
-#define warn(format, ...) printf("\x1b[0;33m[WARN]\x1b[0m " format, #__VA_ARGS__)
+#define info(format, ...) printf("\x1b[0;36m[INFO]\x1b[0m " format, ##__VA_ARGS__)
+#define warn(format, ...) printf("\x1b[0;33m[WARN]\x1b[0m " format, ##__VA_ARGS__)
 
 #define NOT_REACHABLE panic("executed an unreachable statement")
 
