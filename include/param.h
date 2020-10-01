10569488b74f753b0bf6a6bba21e804951824508
Alistair <johnyob132@gmail.com>
2020-10-01 08:51:16 +0100

    Add parameters

---
+++ include/param.h
@@ -0,0 +1,11 @@
+//
+// Created by Alistair O'Brien on 7/13/2020.
+//
+
+#ifndef TINY_OS_PARAM_H
+#define TINY_OS_PARAM_H
+
+#define NCPU                (1)
+#define TIMER_INTERVAL      (10000000)
+
+#endif //TINY_OS_PARAM_H
